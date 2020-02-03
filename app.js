6f7538d6a578994b970ef41e0589049568c30435
Fambirachimwe <t.svaku@gmail.com>
2020-02-03 15:14:51 +0200

    added the browse function

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const App = () => {
     console.log('the app js file')
-};;
+};
 
+
+// adding a new function to the app.js file 
+
+const Browse = () => {
+    console.log('browsing ...')
+}
 
 App();
 
